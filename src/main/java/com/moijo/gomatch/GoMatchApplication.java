9f99a6ab43c3965942a3cc44159723d9ae26d12c
hongyeeun <yeeun5023@gmail.com>
2024-09-27 16:17:22 +0900

    [INIT] 첫 커밋

---
+++ src/main/java/com/moijo/gomatch/GoMatchApplication.java
@@ -0,0 +1,13 @@
+package com.moijo.gomatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoMatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoMatchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/moijo/gomatch/GoMatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moijo.gomatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoMatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
