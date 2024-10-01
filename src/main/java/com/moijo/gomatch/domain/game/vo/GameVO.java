3bf787abfc154c8580982b31f1e395a1c612636f
hongyeeun <yeeun5023@gmail.com>
2024-10-01 16:20:55 +0900

    [ADD] 경기 일정 크롤링 작업중

--- src/main/java/com/moijo/gomatch/domain/game/vo/GameVO.java
+++ src/main/java/com/moijo/gomatch/domain/game/vo/GameVO.java
@@ -1,7 +1,33 @@
 package com.moijo.gomatch.domain.game.vo;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+
+import java.sql.Time;
+import java.sql.Timestamp;
 import java.util.Date;
 
+@Getter
+@Setter
+@Slf4j
+@AllArgsConstructor
 public class GameVO {
-    private Date gameDate;
+    private int gameNo;
+    private String gameDate;
+    private String gameTime;
+    private String teamA;
+    private String vs;
+    private String teamB;
+    private String gameField;
+
+    public GameVO(String gameDate, String gameTime,String teamA,  String vs, String teamB, String gameField) {
+        this.gameDate = gameDate;
+        this.gameTime = gameTime;
+        this.teamA = teamA;
+        this.vs = vs;
+        this.teamB = teamB;
+        this.gameField = gameField;
+    }
 }
