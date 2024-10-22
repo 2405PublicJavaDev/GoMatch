4c34483a42c053d64c534e6a7c1f14463d43eb40
hongyeeun <yeeun5023@gmail.com>
2024-10-22 15:34:29 +0900

    [ADD] 연도별 KBO 타자 순위 추가

--- src/main/java/com/moijo/gomatch/app/game/GameBatchComponent.java
+++ src/main/java/com/moijo/gomatch/app/game/GameBatchComponent.java
@@ -15,9 +15,11 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import org.openqa.selenium.By;
+import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -37,7 +39,11 @@ public class GameBatchComponent {
         this.gameService = gameService;
     }
 
-    // KBO 팀 리그 순위 가져오기
+    /**
+     * 담당자: 홍예은
+     * 관련 기능: [Crawling] 연도별 KBO 팀 리그 순위 가져오기
+     * 설명: 다음 스포츠 크롤링하여 KBO 팀 리그 순위 가져오기
+     */
     public List<String[]> scrapeTeamRank(String yearParam) {
         List<String[]> teamList = new ArrayList<>();  // String 배열 리스트로 데이터 저장
         try {
@@ -72,7 +78,11 @@ public class GameBatchComponent {
         return teamList;
     }
 
-    // 선수 순위 가져오기 - 투수
+    /**
+     * 담당자: 홍예은
+     * 관련 기능: [Crawling] 연도별 KBO 투수 선수 순위 가져오기
+     * 설명: 다음 스포츠 크롤링하여 KBO 투수 선수 순위 가져오기
+     */
     public List<String[]> scrapePitcherRank(String yearParam) {
         List<String[]> pitcherList = new ArrayList<>();
 
@@ -85,10 +95,15 @@ public class GameBatchComponent {
         options.addArguments("--window-size=1920,1080");  // 창 크기 설정
         options.addArguments("--remote-allow-origins=*"); // 모든 오리진 허용
 
+        // pageLoadStrategy 설정 추가
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setCapability("pageLoadStrategy", "none");  // 빠른 페이지 로딩
+        options.merge(caps);
+
         WebDriver driver = new ChromeDriver(options);
         try {
-            driver.get("https://sports.daum.net/record/kbo/pitcher?season=" +yearParam);
-            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+            driver.get("https://sports.daum.net/record/kbo/pitcher?season=" + yearParam);
+            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
             wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("tbl_record")));
             Document doc = Jsoup.parse(driver.getPageSource());
             Elements baseballTeams = doc.select(".tbl_record > tbody > tr");
@@ -123,6 +138,9 @@ public class GameBatchComponent {
                     pitcherList.add(pitcherData);
                 }
             }
+        } catch(TimeoutException e) {
+            System.err.println("페이지 로드가 시간 내 완료되지 않았습니다.");
+
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
@@ -131,8 +149,79 @@ public class GameBatchComponent {
         return pitcherList;
     }
 
+    /**
+     * 담당자: 홍예은
+     * 관련 기능: [Crawling] 연도별 KBO 타자 선수 순위
+     * 설명: 다음 스포츠 크롤링하여 KBO 타자 선수 순위
+     */
+    public List<String[]> scrapeBatterRank(String yearParam) {
+        List<String[]> batterList = new ArrayList<>();
 
-    // 야구 경기 리그 일정 가져오기
+        // 브라우저 창 숨기는 옵션 추가
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");  // Headless 모드
+        options.addArguments("--disable-gpu");  // GPU 비활성화
+        options.addArguments("--no-sandbox");  // Sandbox 모드 비활성화
+        options.addArguments("--disable-dev-shm-usage");  // 메모리 이슈 방지
+        options.addArguments("--window-size=1920,1080");  // 창 크기 설정
+        options.addArguments("--remote-allow-origins=*"); // 모든 오리진 허용
+
+        // pageLoadStrategy 설정 추가
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setCapability("pageLoadStrategy", "none");  // 빠른 페이지 로딩
+        options.merge(caps);
+
+        WebDriver driver = new ChromeDriver(options);
+        try {
+            driver.get("https://sports.daum.net/record/kbo/batter?season=" +yearParam);
+            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(7));
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("tbl_record")));
+            Document doc = Jsoup.parse(driver.getPageSource());
+            Elements baseballTeams = doc.select(".tbl_record > tbody > tr");
+            for (Element baseballTeam : baseballTeams) {
+                Element rank = baseballTeam.selectFirst("td.td_rank"); // 등 수
+                Element name = baseballTeam.selectFirst(".td_name .txt_name"); // 선수이름
+                Element team = baseballTeam.selectFirst(".td_team a"); // 팀 명
+                Element games = baseballTeam.selectFirst("td[data-field='batGp']"); // 경기 수
+                Element batPa = baseballTeam.selectFirst("td[data-field='batPa']"); // 타석
+                Element batAb = baseballTeam.selectFirst("td[data-field='batAb']"); // 타수
+                Element batH = baseballTeam.selectFirst("td[data-field='batH']"); // 안타
+                Element bat2b = baseballTeam.selectFirst("td[data-field='bat2b']"); // 2타
+                Element bat3b = baseballTeam.selectFirst("td[data-field='bat3b']"); // 3타
+                Element batHr = baseballTeam.selectFirst("td[data-field='batHr']");   // 홈런
+                Element batRbi = baseballTeam.selectFirst("td[data-field='batRbi']");  // 타점
+                Element batR = baseballTeam.selectFirst("td[data-field='batR']"); // 득점
+                Element batSb = baseballTeam.selectFirst("td[data-field='batSb']"); // 도루
+                Element batBbhp = baseballTeam.selectFirst("td[data-field='batBbhp']"); // 사사구
+                Element batSo = baseballTeam.selectFirst("td[data-field='batSo']");  // 삼진
+                Element batAvg = baseballTeam.selectFirst("td[data-field='batAvg']"); // 타율
+                Element batObp = baseballTeam.selectFirst("td[data-field='batObp']"); // 출루율
+                Element batSlg = baseballTeam.selectFirst("td[data-field='batSlg']"); // 장타율
+                Element batOps = baseballTeam.selectFirst("td[data-field='batOps']"); // OPS
+
+                if (name != null) {
+                    String[] batterData = {
+                            rank.text(), name.text(), team.text(), games.text(), batPa.text(), batAb.text(),
+                            batH.text(), bat2b.text(), bat3b.text(), batHr.text(), batRbi.text(),
+                            batR.text(), batSb.text(), batBbhp.text(), batSo.text(),
+                            batAvg.text(), batObp.text(), batSlg.text(), batOps.text()
+                    };  // 팀의 정보들을 문자열 배열로 저장한다
+                    batterList.add(batterData);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            driver.quit();  // 크롤링이 끝난 후 브라우저를 닫음
+        }
+        return batterList;
+    }
+
+    /**
+     * 담당자: 홍예은
+     * 관련 기능: [Crawling] 2024년 KBO 경기 일정
+     * 설명: 다음 스포츠 크롤링하여 2024년 KBO 경기 일정/결과 출력
+     */
     public List<GameVO> scrapeSchedule(String dateParam) {
         List<GameVO> gameList = new ArrayList<>();
         // Selenium WebDriver 설정 (ChromeDriver)
@@ -140,17 +229,23 @@ public class GameBatchComponent {
 
         // 브라우저 창 숨기는 옵션 추가
         ChromeOptions options = new ChromeOptions();
-        options.addArguments("--headless=new");
-        options.addArguments("--disable-gpu");
-        options.addArguments("--no-sandbox");
-        options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36");
+        options.addArguments("--headless");  // Headless 모드
+        options.addArguments("--disable-gpu");  // GPU 비활성화
+        options.addArguments("--no-sandbox");  // Sandbox 모드 비활성화
+        options.addArguments("--disable-dev-shm-usage");  // 메모리 이슈 방지
+        options.addArguments("--window-size=1920,1080");  // 창 크기 설정
+        options.addArguments("--remote-allow-origins=*"); // 모든 오리진 허용
+
+        // pageLoadStrategy 설정 추가
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setCapability("pageLoadStrategy", "none");  // 빠른 페이지 로딩
+        options.merge(caps);
 
         WebDriver driver = new ChromeDriver(options);
         try {
             // 셀레니움으로 다음 스포츠 홈페이지 크롤링
             driver.get("https://sports.daum.net/schedule/kbo?date=" + dateParam);
-            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(7));
             wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("scheduleList")));
 
 
@@ -208,7 +303,11 @@ public class GameBatchComponent {
         return gameList;
     }
 
-    // 점수를 int로 파싱
+    /**
+     * 담당자: 홍예은
+     * 관련 기능: [MOD] 점수 형변환
+     * 설명: String으로 크롤링 된 점수를 Integer로 형변환
+     */
     private int parseScore(String score) {
         if (score != null && !score.trim().isEmpty() && !score.equals("-")) {
             try {
@@ -220,7 +319,11 @@ public class GameBatchComponent {
         return 0; // 기본값은 0으로 설정
     }
 
-    // @Scheduled 어노테이션을 통해 매일 9:30에 실행
+    /**
+     * 담당자: 홍예은
+     * 관련 기능: [Update] 경기 정보 업데이트
+     * 설명: 스케줄러 어노테이션을 통해 9:30마다 경기 정보 db 업뎃
+     */
     @Scheduled(cron = "0 30 09 * * ?")
     public void updateGameSchedule() {
         String dateParam = "202410";    // 10월의 경기를 스케줄러로 자동 저장
